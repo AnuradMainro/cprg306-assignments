493ac77cd0f7918803d4f29214dcc191b1f604d1
AnuradMainro <123777065+AnuradMainro@users.noreply.github.com>
2024-01-29 23:18:17 -0700

    Week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Anurad Mainro</p>
+      <Link href="https://github.com/AnuradMainro/cprg306-assignments.git">
+        Link to your GitHub repository
+      </Link>
+    </div>
+  );
+}
